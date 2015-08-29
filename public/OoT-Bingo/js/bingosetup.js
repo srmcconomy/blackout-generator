427012c0a59a0ce671b711e78e1b4247018656ea
Steven McConomy <srmcconomy@gmail.com>
2015-08-29 14:00:15 -0400

    etc

--- public/OoT-Bingo/js/bingosetup.js
+++ public/OoT-Bingo/js/bingosetup.js
@@ -1,20 +1,48 @@
 function bingosetup() {
-  
-	$("#row1").hover(function() { $(".row1").addClass("hover"); }, function() {	$(".row1").removeClass("hover"); });
-	$("#row2").hover(function() { $(".row2").addClass("hover"); }, function() {	$(".row2").removeClass("hover"); });
-	$("#row3").hover(function() { $(".row3").addClass("hover"); }, function() {	$(".row3").removeClass("hover"); });
-	$("#row4").hover(function() { $(".row4").addClass("hover"); }, function() {	$(".row4").removeClass("hover"); });
-	$("#row5").hover(function() { $(".row5").addClass("hover"); }, function() {	$(".row5").removeClass("hover"); });
-
-	$("#col1").hover(function() { $(".col1").addClass("hover"); }, function() {	$(".col1").removeClass("hover"); });
-	$("#col2").hover(function() { $(".col2").addClass("hover"); }, function() {	$(".col2").removeClass("hover"); });
-	$("#col3").hover(function() { $(".col3").addClass("hover"); }, function() {	$(".col3").removeClass("hover"); });
-	$("#col4").hover(function() { $(".col4").addClass("hover"); }, function() {	$(".col4").removeClass("hover"); });
-	$("#col5").hover(function() { $(".col5").addClass("hover"); }, function() {	$(".col5").removeClass("hover"); });
-
-	$("#tlbr").hover(function() { $(".tlbr").addClass("hover"); }, function() {	$(".tlbr").removeClass("hover"); });
-	$("#bltr").hover(function() { $(".bltr").addClass("hover"); }, function() {	$(".bltr").removeClass("hover"); });
-
+	var bingoOpts = {
+		seed: getUrlParameter('seed') || Math.ceil(999999 * Math.random()).toString(),
+		mode: getUrlParameter('mode') || 'normal',
+		lang: getUrlParameter('lang') || 'name'
+	};
+	
+	var bingoFunc = ootBingoGenerator;
+	var bad = false;
+	var regexes = [ {regex: /\d(?: different)? unused keys in Gerudo Training Grounds/, count: 0},
+					{regex: /\d Maps/, count: 0},
+					{regex: /\d Compasses/, count: 0},
+					{regex: /\d Hearts/, count: 0},
+					{regex: /At least \d songs/, count: 0},
+					{regex: /(?:3 Shields & 3 [^\s]+|3 [^\s]+ & 3 Shields)/, count: 0},
+					{regex: /(?:3 Swords & 3 [^\s]+|3 [^\s]+ & 3 Swords)/, count: 0},
+					{regex: /(?:All 5 Skulltulas in Shadow Temple|At least 4 Skulltulas in Shadow Temple)/, count: 0},
+					{regex: /(?:All 5 Skulltulas in Water Temple|At least 3 Skulltulas in Water Temple)/, count: 0},
+					{regex: /At least \d Magic Beans/, count: 0}
+				];
+	do {
+		bad = false;
+		for (var r of regexes) r.count = 0;
+		bingoOpts.seed = Math.ceil(999999 * Math.random()).toString();
+		var bingoBoard = bingoFunc(bingoList, bingoOpts);
+		if(bingoBoard) {
+			for (i=1; i<=25; i++) {
+				for(var r of regexes) {
+					if (r.regex.exec(bingoBoard[i].name) != null) {r.count++;console.log(r);}
+					if (r.count > 1) bad = true;
+					
+				}
+				for (j=1; j<=25; j++) {
+					if (i == j) continue;
+					if (bingoBoard[i].name == bingoBoard[j].name) {bad = true; console.log(bingoBoard[i]);}
+				}
+			}
+		} else {
+			bad = true;
+		}
+	} while(bad);
+	console.log(bingoOpts.seed);
+	$("#seed").text(bingoOpts.seed);
+	$("#link").text("http://www.speedrunslive.com/tools/oot-bingo/?seed=" + bingoOpts.seed)
+	$("#link").attr("href", "http://www.speedrunslive.com/tools/oot-bingo/?seed=" + bingoOpts.seed)
 }
 
 $(bingosetup);
