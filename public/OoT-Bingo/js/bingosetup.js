fae2979adca16238f7c821da52f7cd5c991cd91c
Steven McConomy <srmcconomy@gmail.com>
2015-08-29 14:53:43 -0400

    added tunics and strength

--- public/OoT-Bingo/js/bingosetup.js
+++ public/OoT-Bingo/js/bingosetup.js
@@ -7,17 +7,19 @@ function bingosetup() {
 	
 	var bingoFunc = ootBingoGenerator;
 	var bad = false;
-	var regexes = [ {regex: /\d(?: different)? unused keys in Gerudo Training Grounds/, count: 0},
-					{regex: /\d Maps/, count: 0},
-					{regex: /\d Compasses/, count: 0},
-					{regex: /\d Hearts/, count: 0},
-					{regex: /At least \d songs/, count: 0},
-					{regex: /(?:3 Shields & 3 [^\s]+|3 [^\s]+ & 3 Shields)/, count: 0},
-					{regex: /(?:3 Swords & 3 [^\s]+|3 [^\s]+ & 3 Swords)/, count: 0},
-					{regex: /(?:3 Boots & 3 [^\s]+|3 [^\s]+ & 3 Boots)/, count: 0},
-					{regex: /(?:All 5 Skulltulas in Shadow Temple|At least 4 Skulltulas in Shadow Temple)/, count: 0},
-					{regex: /(?:All 5 Skulltulas in Water Temple|At least 3 Skulltulas in Water Temple)/, count: 0},
-					{regex: /At least \d Magic Beans/, count: 0}
+	var regexes = [ {regex: /\d(?: different)? unused keys in Gerudo Training Grounds/, maxcount: 1, count: 0},
+					{regex: /\d Maps/, maxcount: 1, count: 0},
+					{regex: /\d Compasses/, maxcount: 1, count: 0},
+					{regex: /\d Hearts/, maxcount: 1, count: 0},
+					{regex: /At least \d songs/, maxcount: 1, count: 0},
+					{regex: /(?:3 Shields & 3 [^\s]+|3 [^\s]+ & 3 Shields)/, maxcount: 1, count: 0},
+					{regex: /(?:3 Swords & 3 [^\s]+|3 [^\s]+ & 3 Swords)/, maxcount: 1, count: 0},
+					{regex: /(?:3 Boots|3 Boots & 3 [^\s]+|3 [^\s]+ & 3 Boots)/, maxcount: 1, count: 0},
+					{regex: /(?:3 Tunics|3 Tunics & 3 [^\s]+|3 [^\s]+ & 3 Tunics)/, maxcount: 1, count: 0},
+					{regex: /(?:All 5 Skulltulas in Shadow Temple|At least 4 Skulltulas in Shadow Temple)/, maxcount: 1, count: 0},
+					{regex: /(?:All 5 Skulltulas in Water Temple|At least 3 Skulltulas in Water Temple)/, maxcount: 1, count: 0},
+					{regex: /At least \d Magic Beans/, maxcount: 1, count: 0},
+					{regex: /(?:[^\s]+ Gauntlets|Goron Bracelet)/, maxcount: 3, count: 0}
 				];
 	do {
 		bad = false;
@@ -28,7 +30,7 @@ function bingosetup() {
 			for (i=1; i<=25; i++) {
 				for(var r of regexes) {
 					if (r.regex.exec(bingoBoard[i].name) != null) {r.count++;console.log(r);}
-					if (r.count > 1) bad = true;
+					if (r.count > r.maxcount) bad = true;
 					
 				}
 				for (j=1; j<=25; j++) {
